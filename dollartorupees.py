db36ec8255aada7156627a70b3dabe291ca27b0c
Muhammad Raiyaan <83336994+muhammedraiyaan2@users.noreply.github.com>
2021-06-22 21:17:24 +0530

    Create dollartorupees.py

---
+++ dollartorupees.py
@@ -0,0 +1,6 @@
+#here I print some message
+print("Welcome to the dollar to india ruppes converter")
+a=int(input("Enter the dollar"))
+#here I save the int input as a variable name a
+print(a*74.37)
+#finally I print the output
